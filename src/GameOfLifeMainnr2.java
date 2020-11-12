1bcfd0febe356469640993887e209b5b4ce0c8b1
Inocxh <Inocxh@gmail.com>
2020-11-12 13:05:53 +0100

    Improved perfomance and made GOL run on the correct version of StdDraw

--- src/GameOfLifeMainnr2.java
+++ src/GameOfLifeMainnr2.java
@@ -2,64 +2,43 @@ import java.util.*;
 import java.io.*;
 
 public class GameOfLifeMainnr2 {
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
-		int lineNr = 0;
+		
 		Scanner console = new Scanner(System.in);
+		int[][] matrixData = getInput(console);
+
+		GameOfLife l = new GameOfLife(matrixData);
+		l.setGrid();
+		l.initializeGrid();
+		while (true) {
+			l.nextState();
+		}
+
+	}
+
+	private static int[][] getInput(Scanner console) throws FileNotFoundException {
+		int lineCounter = 0;
+	
 		String file = console.next();
 		Scanner input = new Scanner(new File(file));
-		while(input.hasNextLine()) {
+		while (input.hasNextLine()) {
 			String line = input.nextLine();
-			lineNr++;
+			lineCounter++;
 		}
-		
-		int [][] fileMatrix = new int[lineNr][lineNr];
-		
+
+		int[][] fileMatrix = new int[lineCounter][lineCounter];
+
 		Scanner input2 = new Scanner(new File(file));
-		for(int i = 0; i< lineNr; i++) {
-			for(int j = 0; j<lineNr; j++) {
-				if(input2.hasNextInt()) {
+		for (int i = 0; i < lineCounter; i++) {
+			for (int j = 0; j < lineCounter; j++) {
+				if (input2.hasNextInt()) {
 					fileMatrix[i][j] = input2.nextInt();
 				}
 			}
 		}
 		
-		printing(fileMatrix);
-		
-		//int[][] test = {
-				//{0,0,0,0,0,0,1,0,1,1,0,1},
-				//{0,0,0,0,0,0,0,1,0,0,0,1},
-				//{0,0,1,1,1,0,1,1,1,1,1,0},
-				//{0,1,1,1,0,0,1,0,1,1,0,1},
-				//{0,0,0,0,0,0,0,0,0,0,0,0},
-				//{0,0,0,0,0,0,1,1,1,0,1,1},
-				//{0,0,0,0,0,0,0,0,1,0,1,1},
-				//{0,0,0,0,0,0,0,0,0,1,1,0},
-				//{0,0,1,1,1,0,1,1,1,0,0,0},
-				//{0,1,1,1,0,0,1,1,0,0,1,1},
-				//{0,1,0,1,0,0,0,1,1,0,0,0},
-				//{0,0,0,0,0,0,1,1,1,1,0,1}
-				//
-		//};
-		
-		GameOfLife l = new GameOfLife(fileMatrix);
-		l.setGrid();
-		l.initializeGrid();
-		while(true) {
-			l.nextState();
-		}
-			
+		return fileMatrix;
 	}
-		
-		
-		public static void printing(int[][] matrix){
-			for(int i = 0; i < matrix.length; i++) {
-				for(int j = 0; j < matrix[i].length; j++) {
-					System.out.print(matrix[i][j] + " ");
-				}
-				System.out.println();
-			}
-			System.out.println();
-	}
-	
+
 }
