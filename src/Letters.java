2571c363cf9bd802ff039c70dd3e77917bb6ca32
Inocxh <Inocxh@gmail.com>
2020-11-15 14:53:42 +0100

    Made Letters correct

--- src/Letters.java
+++ src/Letters.java
@@ -1,19 +1,10 @@
-import java.util.Scanner;
 
 public class Letters {
-
 	public static void main(String[] args) {
-		System.out.println(countLetters());
-	}
-	public static int countLetters() {
-		Scanner console = new Scanner(System.in);
-		String input = console.nextLine();
-		int antalLet = 0;
-		for(int i = 0; i<input.length(); i++) {
-			if(input.charAt(i)!=' ') {
-				antalLet++;
-			}
+		int out = 0;
+		for (String i: args) {
+			out += i.length();
 		}
-		return antalLet;
+		System.out.printf("%d",out);
 	}
 }
